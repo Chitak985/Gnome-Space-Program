0eee20041a880fff0183d33a2977c12b75d22e3f
Sushutt <andyduha1981@gmail.com>
2025-05-05 21:11:44 -0700

    Test Hyperbolic Orbits

--- Scripts/OrbitalMechanics/OrbitTester.cs
+++ Scripts/OrbitalMechanics/OrbitTester.cs
@@ -4,27 +4,51 @@ using System.Collections.Generic;
 
 public partial class OrbitTester : Node3D
 {
+    [Export] public double testBodySMA = 2;
+    [Export] public double testBodyECC = 0;
+    [Export] public double timeSpeed = 1;
+    [Export] public double time;
     public List<CelestialBody> cBodyList = [];
+
+    public CelestialBody anotherBody;
 
     public override void _Ready()
     {
         CelestialBody rootBody = new CelestialBody{
             mass = 10000000000000,
+            orbit = new Orbit{
+                position = new Double3(0,0,0)
+            }
         };
         cBodyList.Add(rootBody);
 
-        cBodyList.Add(new CelestialBody{
-            mass = 10000,
+        anotherBody = new CelestialBody{
+            mass = 1000000000000,
             orbit = new Orbit{
                 parent = rootBody,
                 semiMajorAxis = 10,
                 inclination = 0,
-                eccentricity = .5,
+                eccentricity = 1.1,
                 argumentOfPeriapsis = 0,
                 longitudeOfAscendingNode = 0,
                 trueAnomaly = 0
             }
-        });
+        };
+        cBodyList.Add(anotherBody);
+
+        CelestialBody moon = new CelestialBody{
+            mass = 10000,
+            orbit = new Orbit{
+                parent = anotherBody,
+                semiMajorAxis = 4,
+                inclination = 90,
+                eccentricity = 0,
+                argumentOfPeriapsis = 0,
+                longitudeOfAscendingNode = 0,
+                trueAnomaly = 0
+            }
+        };
+        //cBodyList.Add(moon);
         
         foreach (CelestialBody cBody in cBodyList)
         {
@@ -34,13 +58,18 @@ public partial class OrbitTester : Node3D
 
     public override void _Process(double delta)
     {
+        anotherBody.orbit.semiMajorAxis = testBodySMA*10;
+        anotherBody.orbit.eccentricity = testBodyECC;
         foreach (CelestialBody cBody in cBodyList)
         {
-            if (cBody.orbit != null)
+            if (cBody.orbit.parent != null)
             {
-                (Double3 position, Double3 velocity) = PatchedConics.KOEtoECI(cBody.orbit, cBody.orbit.parent, Time.GetUnixTimeFromSystem(), 0);
-                GD.Print(position.X + " " + position.Y + " " + position.Z);
-                cBody.debugOrb.GlobalPosition = new Vector3((float)position.X,(float)position.Y,(float)position.Z);
+                (Double3 position, Double3 velocity) = PatchedConics.KOEtoECI(cBody.orbit, cBody.orbit.parent, Time.GetUnixTimeFromSystem()*timeSpeed, 0);
+
+                cBody.orbit.position = position;
+                Double3 finalPos = position + cBody.orbit.parent.orbit.position;
+                
+                cBody.debugOrb.GlobalPosition = new Vector3((float)finalPos.X,(float)finalPos.Y,(float)finalPos.Z);
             }
         }
     }
--- Scripts/OrbitalMechanics/PatchedConics.cs
+++ Scripts/OrbitalMechanics/PatchedConics.cs
@@ -1,9 +1,7 @@
 using Godot;
 using System;
-using System.Numerics;
-using System.Runtime.InteropServices;
 
-// All-encompassing class for everything patched-conics related
+// All-encompassing class for orbital math
 /*
 Sources used:
 https://en.wikipedia.org/wiki/Earth-centered_inertial
@@ -11,6 +9,10 @@ https://www.sciencedirect.com/topics/engineering/patched-conic
 https://ai-solutions.com/_freeflyeruniversityguide/patched_conics_transfer.htm#calculatingapatchedconicsproblem
 https://www.mathworks.com/help/aerotbx/ug/keplerian2ijk.html
 https://space.stackexchange.com/questions/19322/converting-orbital-elements-to-cartesian-state-vectors
+https://space.stackexchange.com/questions/24646/finding-x-y-z-vx-vy-vz-from-hyperbolic-orbital-elements
+
+Handy list of orbital formulas:
+https://www.bogan.ca/orbits/kepler/orbteqtn.html
 */
 public partial class PatchedConics : Node
 {
@@ -23,38 +25,56 @@ public partial class PatchedConics : Node
         double MU = GravConstant * parent.mass;
 
         // Compile our favourite Keplerian orbit elements
-        double SMA = orbit.semiMajorAxis;
         double ECC = orbit.eccentricity;
+
+        double SMA = orbit.semiMajorAxis;
         double INC = orbit.inclination;
         double AOP = orbit.argumentOfPeriapsis;
         double LAN = orbit.longitudeOfAscendingNode;
-        double TRA = orbit.trueAnomaly;
+        //double TRA = orbit.trueAnomaly;
 
-        // Compute the mean anomaly
-        double PRD = 2 * Mathf.Pi * Mathf.Sqrt(SMA * SMA * SMA / MU); //Orbital period
-        double n = Math.Sqrt(MU/Math.Pow(SMA,3));
-        double M = n*(time-PRD);
+        double V;
+        double radius;
+        double H;
+        double p;
 
-        double EA = GetEccentricAnomaly(M, ECC);
-        double MA = EA - ECC * Math.Sin(EA);
+        if (ECC < 1)
+        {
+            // Compute the mean anomaly
+            double PRD = 2 * Math.PI * Math.Sqrt(SMA * SMA * SMA / MU); //Orbital period
+            double n = Math.Sqrt(MU/Math.Pow(SMA,3));
+            double M = n*(time-PRD);
+            double EA = GetEccentricAnomaly(M, ECC);
 
-        // What the fuck??
-        double V = 2*Math.Atan(Math.Sqrt((1+ECC)/(1-ECC)) * Math.Tan(EA/2)); // V
-        // Step four
-        double radius = SMA*(1 - ECC*Math.Cos(EA));
-        // Step five
-        double H = Math.Sqrt(MU*SMA * (1 - Math.Pow(ECC,2)));
-        // Step SIX!?!??
-        // I know I just stole this shit from stackexchange but why the fuck are you doing this?
+            //double MA = EA - ECC * Math.Sin(EA);
+            V = 2 * Math.Atan(Math.Sqrt((1+ECC)/(1-ECC)) * Math.Tan(EA/2)); // V
+            // Step four
+            radius = SMA * (1 - ECC*Math.Cos(EA));
+            // PART FIVE! BOOBYTRAP THE LETTER H!!!
+            H = Math.Sqrt(MU*SMA * (1 - Math.Pow(ECC,2)));
+            // Step SIX!?!?? WHERE IS STEP SIX?
+            // PART SEVEN!!!! ANGULAR MOMENTUM!
+            p = SMA * (1 - Math.Pow(ECC,2));
+        }else{
+            // Compute the mean anomaly HYPERBOLIC EDITION
+            double n = Math.Sqrt(MU/Math.Pow(Math.Abs(SMA),3));
+            double M = n*time;
+            double EA = GetHyperbolicAnomaly(M,ECC);
+
+            // treu naomely
+            
+            H = Math.Sqrt(MU * Math.Abs(SMA) * (ECC * ECC - 1));
+            V = 2 * Math.Atan(Math.Sqrt((ECC + 1) / (ECC - 1)) * Math.Tanh(EA / 2));
+            p = Math.Abs(SMA) * (ECC * ECC - 1);
+            radius = p / (1 + ECC * Math.Cos(V));
+        }
+        
         double Om = LAN;
         double w =  AOP;
 
         double X = radius*(Math.Cos(Om)*Math.Cos(w+V) - Math.Sin(Om)*Math.Sin(w+V)*Math.Cos(INC));
         double Y = radius*(Math.Sin(Om)*Math.Cos(w+V) + Math.Cos(Om)*Math.Sin(w+V)*Math.Cos(INC));
         double Z = radius*(Math.Sin(INC)*Math.Sin(w+V));
-
-        // STEP SEVEN!!!!
-        double p = SMA*(1-Math.Pow(ECC,2)); // The hell is p???? Imagine a world where mathematicians explained what the fuck these variables are
 
         double V_X = X*H*ECC/(radius*p)*Math.Sin(V) - H/radius*(Math.Cos(Om)*Math.Sin(w+V) + Math.Sin(Om)*Math.Cos(w+V)*Math.Cos(INC));
         double V_Y = Y*H*ECC/(radius*p)*Math.Sin(V) - H/radius*(Math.Sin(Om)*Math.Sin(w+V) - Math.Cos(Om)*Math.Cos(w+V)*Math.Cos(INC));
@@ -64,11 +84,16 @@ public partial class PatchedConics : Node
     }
 
     // Keplerian method of calculating eccentric anomaly apparently
-    public static double GetEccentricAnomaly(double meanAnomaly, double eccentricity, double tolerance = 1e-6, int maxIter = 100)
+    public static double GetEccentricAnomaly(double meanAnomaly, double eccentricity, double tolerance = 1e-8, int maxIter = 100000)
     {
         double E;
 
-        if (eccentricity < 0.8) E = meanAnomaly; else E = Math.PI;
+        if (eccentricity > 0.8){
+            E = Math.PI;
+        }else{
+            E = meanAnomaly;
+        }
+
         for (int i = 0; i < maxIter; i++)
         {
             double delta = (E - eccentricity * Math.Sin(E) - meanAnomaly) / (1 - eccentricity * Math.Cos(E));
@@ -80,6 +105,22 @@ public partial class PatchedConics : Node
         }
             
         return E;
+    }
+
+    // Solve for hyperbolic eccentric anomaly NOT H using Newton-Raphson
+    public static double GetHyperbolicAnomaly(double meanAnomaly, double eccentricity, double tolerance = 1e-8, int maxIter = 100000)
+    {
+        double H = Math.Log(2 * Math.Abs(meanAnomaly) / eccentricity + 1.8); // Initial guess
+        for (int i = 0; i < maxIter; i++)
+        {
+            double f = eccentricity * Math.Sinh(H) - H - meanAnomaly;
+            double fp = eccentricity * Math.Cosh(H) - 1;
+            double dH = f / fp;
+            H -= dH;
+            if (Math.Abs(dH) < tolerance)
+                break;
+        }
+        return H;
     }
 }
 
@@ -94,4 +135,7 @@ public class Orbit
     public double argumentOfPeriapsis;
     public double longitudeOfAscendingNode;
     public double trueAnomaly;
+
+    public Double3 position;
+    public Double3 velocity;
 }
